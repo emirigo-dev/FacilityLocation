5618590108db97d1767619166b30c00b1eb1bcb9
unknown <emirigobello@gmail.com>
2020-11-27 18:02:22 -0300

    Inicio de proyecto

---
+++ src/FacilyLocation/Persona.java
@@ -0,0 +1,28 @@
+package FacilyLocation;
+
+public class Persona {
+
+	private String _nombre;
+	private double _latitud;
+	private double _longitud;
+	
+	public Persona(String nombre, double latitud, double longitud)
+	{
+		_nombre = nombre;
+		_latitud = latitud;
+		_longitud = longitud;
+	}
+	
+	public double get_longitud() {
+		return _longitud;
+	}
+
+	public double get_latitud() {
+		return _latitud;
+	}
+
+	public String get_nombre() {
+		return _nombre;
+	}
+
+}
